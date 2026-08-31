f81ad4a819c69c73b9b38a965fc293c8966fdba9
Marakuja15 <piotrswistak8@gmail.com>
2026-09-01 00:17:28 +0200

    Dodaj pliki projektów.

---
+++ ECSengine/ECSengine.cpp
@@ -0,0 +1,24 @@
+﻿// ECSengine.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "ECSengine.h"
+#include "raylib.h"
+
+using namespace std;
+
+int main()
+{
+    
+    InitWindow(800, 450, "Dziala!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib dziala!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
